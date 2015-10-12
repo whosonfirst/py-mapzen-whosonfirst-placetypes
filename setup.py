56d4d5c656000124db8e1241ad024580399f3440
thisisaaroland <aaron@mapzen.com>
2015-10-12 10:58:31 -0700

    update spec to account for localadmin and macroregion/county

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ desc = open("README.md").read(),
 setup(
     name='mapzen.whosonfirst.placetypes',
     namespace_packages=['mapzen', 'mapzen.whosonfirst', 'mapzen.whosonfirst.placetypes'],
-    version='0.04',
+    version='0.05',
     description='Simple Python wrapper for managing Who\'s On First placetypes',
     author='Mapzen',
     url='https://github.com/whosonfirst/py-mapzen-whosonfirst-placetypes',
@@ -19,5 +19,5 @@ setup(
     packages=packages,
     scripts=[
         ],
-    download_url='https://github.com/whosonfirst/py-mapzen-whosonfirst-placetypes/releases/tag/v0.04',
+    download_url='https://github.com/whosonfirst/py-mapzen-whosonfirst-placetypes/releases/tag/v0.05',
     license='BSD')
