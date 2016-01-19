43f059e6aa453162a79667da8cae252c3c24e60c
Dan Phiffer <dan@phiffer.org>
2016-01-19 15:15:49 -0800

    🔧 Updated setup.py to use VERSION

--- setup.py
+++ setup.py
@@ -1,14 +1,26 @@
 #!/usr/bin/env python
 
+# Remove .egg-info directory if it exists, to avoid dependency problems with
+# partially-installed packages (20160119/dphiffer)
+
+import os, sys
+from shutil import rmtree
+
+cwd = os.path.dirname(os.path.realpath(sys.argv[0]))
+egg_info = cwd + "/mapzen.whosonfirst.placetypes.egg-info"
+if os.path.exists(egg_info):
+    rmtree(egg_info)
+
 from setuptools import setup, find_packages
 
 packages = find_packages()
-desc = open("README.md").read(),
+desc = open("README.md").read()
+version = open("VERSION").read()
 
 setup(
     name='mapzen.whosonfirst.placetypes',
     namespace_packages=['mapzen', 'mapzen.whosonfirst', 'mapzen.whosonfirst.placetypes'],
-    version='0.11',
+    version=version,
     description='Simple Python wrapper for managing Who\'s On First placetypes',
     author='Mapzen',
     url='https://github.com/whosonfirst/py-mapzen-whosonfirst-placetypes',
@@ -24,5 +36,5 @@ setup(
     scripts=[
         'scripts/wof-graph-placetypes'
         ],
-    download_url='https://github.com/whosonfirst/py-mapzen-whosonfirst-placetypes/releases/tag/v0.11',
+    download_url='https://github.com/whosonfirst/py-mapzen-whosonfirst-placetypes/releases/tag/' + version,
     license='BSD')
